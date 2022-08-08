dbedba60bd726e7d6b4dda645511f9d2361c7a5e
sidchopp <sidchopp@gmail.com>
2022-08-08 15:34:49 -0600

    Footer comp created

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div>
+      {" "}
+      <a
+        href="https://sid-projects.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        My Other Projects
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
+
+//components
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -15,53 +17,19 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          My first <a href="https://nextjs.org">Next.js!</a> project
+          My first{" "}
+          <a
+            href="https://nextjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Next.js!
+          </a>{" "}
+          project
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
       <footer className={styles.footer}>
-        <a
-          href="https://sid-projects.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Other Projects
-        </a>
+        <Footer />
       </footer>
     </div>
   );
